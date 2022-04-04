76e20def136c559732cb8e5d7ee197fe4e02908a
Syd Amir <hemedani@gmail.com>
2022-04-04 21:27:04 +0430

    modify acts to have dynamic and static

--- acts/acts.ts
+++ acts/acts.ts
@@ -1,53 +1,140 @@
 import { SchemasKey } from "../models/mod.ts";
 
 // const actsSample = {
-//   user: {
-//     create: {
-//       validator: (input: any) => {
-//         return true;
+//   dynamic: {
+//     user: {
+//       create: {
+//         validator: (input: any) => {
+//           return true;
+//         },
+//         fn: (input: any) => {
+//           return input;
+//         },
 //       },
-//       fn: (input: any) => {
-//         return input;
-//       }
+//       update: {
+//         validator: (input: any) => {
+//           return true;
+//         },
+//         fn: (input: any) => {
+//           return input;
+//         },
+//       },
 //     },
-//     update: {
-//       validator: (input: any) => {
-//         return true;
+//   },
+//   static: {
+//     "blogFirstPage": {
+//       "get": {
+//         "validator": (input: any) => {
+//           return true;
+//         },
+//         "fn": (input: any) => {
+//           return input;
+//         },
 //       },
-//       fn: (input: any) => {
-//         return input;
-//       }
-//     }
-//   }
-// }
+//       "set": {
+//         "validator": (input: any) => {
+//           return true;
+//         },
+//         "fn": (input: any) => {
+//           return input;
+//         },
+//       },
+//     },
+//   },
+// };
 
 export interface Act {
   validator: Function;
   fn: Function;
 }
 
-export const acts: Record<SchemasKey, Record<string, Act>> = {};
+export interface Acts {
+  dynamic: {
+    [key: string]: {
+      [key: string]: Act;
+    };
+  };
+  static: {
+    [key: string]: {
+      [key: string]: Act;
+    };
+  };
+}
+
+export const acts: Acts = {
+  dynamic: {},
+  static: {},
+};
 
 export interface ActInp {
+  type: "static" | "dynamic";
   schema: SchemasKey;
   actName: string;
   validator: Function;
   fn: Function;
 }
 
-export const setAct: (doitInp: ActInp) => void = (
-  { schema, actName, validator, fn },
+export const setAct: (actInp: ActInp) => void = (
+  { type, schema, actName, validator, fn },
 ) => {
-  acts[schema][actName] = { validator, fn };
+  if (!acts[type]) {
+    throw new Error(`Invalid type: ${type}`);
+  }
+  if (!acts[type][schema]) {
+    acts[type][schema] = {};
+  }
+  acts[type][schema][actName] = {
+    validator,
+    fn,
+  };
 };
 
-export const getAct: (
+export const getDynamicActs = () => acts.dynamic;
+
+export const getStaticActs = () => acts.static;
+
+export const getSchemaDynamicActs: (
+  schema: SchemasKey,
+) => { [key: string]: Act } = (
+  schema,
+) => {
+  if (!acts.dynamic[schema]) {
+    return {};
+  }
+  return acts.dynamic[schema];
+};
+
+export const getSchemaStaticActs: (schema: string) => { [key: string]: Act } = (
+  schema,
+) => {
+  if (!acts.static[schema]) {
+    throw new Error(`Invalid schema: ${schema}`);
+  }
+  return acts.static[schema];
+};
+
+export const getDynamicAct: (
   schema: SchemasKey,
   actName: string,
-) => Act | undefined = (schema, actName) => {
-  return acts[schema][actName];
+) => Act = (schema, actName) => {
+  if (!acts.dynamic[schema]) {
+    throw new Error(`Invalid schema: ${schema}`);
+  }
+  if (!acts.dynamic[schema][actName]) {
+    throw new Error(`Invalid actName: ${actName}`);
+  }
+  return acts.dynamic[schema][actName];
 };
 
-export const getActs: (schema: SchemasKey) => Act[] = (schema) => {
-  return Object.values(acts[schema]);
+export const getStaticAct: (
+  schema: string,
+  actName: string,
+) => Act = (schema, actName) => {
+  if (!acts.static[schema]) {
+    throw new Error(`Invalid actName: ${actName}`);
+  }
+  if (!acts.static[schema][actName]) {
+    throw new Error(`Invalid actName: ${actName}`);
+  }
+  return acts.static[schema][actName];
 };
