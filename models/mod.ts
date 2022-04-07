e64cc39d0b9cd0e60c115bb31396e9d7779d16ff
Syd Amir <hemedani@gmail.com>
2022-04-07 23:09:49 +0430

    add select struct to mod

--- models/mod.ts
+++ models/mod.ts
@@ -3,4 +3,5 @@ export * from "./outrelation.ts";
 export * from "./pure.ts";
 export * from "./relation.ts";
 export * from "./schema.ts";
+export * from "./selectStruct.ts";
 export * from "./types.ts";
