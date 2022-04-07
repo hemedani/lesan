56530b85d5d562530fe90a56aed1c173f41b778a
Syd Amir <hemedani@gmail.com>
2022-04-07 23:12:47 +0430

    fixed validation on runAct

--- utils/serveLesan.ts
+++ utils/serveLesan.ts
@@ -4,13 +4,16 @@ import { Body, parsBody } from "./mod.ts";
 
 export const runAct = (body: Body) => {
   const act = getAct(body.contents, body.wants.model, body.wants.act);
-  assert(body.wants.act, act.validator);
+  assert(body.details, act.validator);
+
   return act.fn(body);
 };
 
 const checkActs = (body: Body) => {
-  const acts = enums(getActsKeys(body.contents, body.wants.model));
+  const actKeys = getActsKeys(body.contents, body.wants.model);
+  const acts = enums(actKeys);
   assert(body.wants.act, acts);
+
   return runAct(body);
 };
 
@@ -19,14 +22,17 @@ const checkModels = (body: Body) => {
     ? enums(getStaticKeys())
     : enums(getDynamicKeys());
   assert(body.wants.model, models);
+
   return checkActs(body);
 };
 
 const checkContetType = (body: Body) => {
   const content = enums(["dynamic", "static"]);
   assert(body.contents, content);
+
   return checkModels(body);
 };
+
 export const serveLesan = async (req: Request) => {
   const response = async () => {
     const body = (await parsBody(req)) as Body;
